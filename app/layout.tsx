8f991a6a8b127ae1b3be281886ac5090951f1586
ayoolaowoilu <ayoolaowoilu@gmail.com>
2026-07-21 00:50:21 +0100

    Chore : Updated the pub folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-         <script src="https://5gvci.com/act/files/tag.min.js?z=11361214" data-cfasync="false" async></script>
+   <script src="https://quge5.com/88/tag.min.js" data-zone="261976" async data-cfasync="false"></script>
          {/* <script src="https://quge5.com/88/tag.min.js" data-zone="261976" async data-cfasync="false"></script> */}
          
       </head>
