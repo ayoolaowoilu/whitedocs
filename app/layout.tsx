ef27c6fc2395e4324ed867b7d5324da6870a57fe
ayoolaowoilu <ayoolaowoilu@gmail.com>
2026-07-21 00:33:55 +0100

    Chore : Edited the root dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
       </head>
       <body className="min-h-full flex flex-col">{children}
 
-        {/* <AdPopup delay={5000} zoneId="1234567" showAgainAfterHours={0.01} /> */}
+        <AdPopup delay={5000} zoneId="11361214" showAgainAfterHours={0.01} />
       </body>
     </html>
   );
